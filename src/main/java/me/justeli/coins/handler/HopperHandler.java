e709c380fc1801b77db57ee10b072a4ec481882b
AllF1RE <111210671+AllFiRE0@users.noreply.github.com>
2026-06-14 20:36:33 +0300

    Cancel event for coin items in HopperHandler

--- src/main/java/me/justeli/coins/handler/HopperHandler.java
+++ src/main/java/me/justeli/coins/handler/HopperHandler.java
@@ -30,10 +30,7 @@ public final class HopperHandler implements Listener {
         }
 
         ItemStack item = event.getItem().getItemStack();
-        if (!coins.getCoinMeta().isDroppedCoin(item)) {
-            return;
+        if (coins.getCoinMeta().isCoin(item)) {
+            event.setCancelled(true);
         }
-
-        event.setCancelled(true);
-    }
 }
